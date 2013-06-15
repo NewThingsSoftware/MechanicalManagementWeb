9b4d479734e0ae35717e5361de2a3fcc9629a200
Bruno <Bruno@BrunoPC.unoesc.lan>
2013-06-15 19:34:44 -0300

    Cliente

--- MechanicalManagementWeb/src/java/classes/Cliente.java
+++ MechanicalManagementWeb/src/java/classes/Cliente.java
@@ -1,13 +1,55 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package classes;
 
-/**
- *
- * @author Bruno
- */
 public class Cliente {
+    private int codCliente;
+    private String nome;
+    private String cpf;
+    private String rg;
+
+    public Cliente(String nome, String cpf, String rg) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    public Cliente(int codCliente, String nome, String cpf, String rg) {
+        this.codCliente = codCliente;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    public int getCodCliente() {
+        return codCliente;
+    }
+
+    public void setCodCliente(int codCliente) {
+        this.codCliente = codCliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+    
     
 }
--- MechanicalManagementWeb/src/java/daos/ClienteDAO.java
+++ MechanicalManagementWeb/src/java/daos/ClienteDAO.java
@@ -1,13 +1,64 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package daos;
 
-/**
- *
- * @author Bruno
- */
+import conecta.Conecta;
+import classes.Cliente;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClienteDAO {
+
+    private Conecta conecta;
+
+    public ClienteDAO() {
+        conecta = new Conecta();
+    }
     
+    public void incluirCliente(Cliente cliente)
+    {
+        
+    }
+    
+    public void alterarCliente(Cliente cliente){
+        
+    }
+
+    public List<Cliente> obterTodos() {
+        if ("sucesso".equals(conecta.getMsg())) {
+            try {
+                String sql = "SELECT * FROM APP.A1_CLIENTE ORDER BY NOME";
+                ResultSet rs = conecta.getStm().executeQuery(sql);
+                List<Cliente> clientes = new ArrayList<Cliente>();
+                while (rs.next()) {
+                    clientes.add(new Cliente(rs.getInt("COD_CLIENTE"),
+                            rs.getString("NOME"),
+                            rs.getString("CPF"),
+                            rs.getString("RG")));
+                }
+                return clientes;
+            } catch (SQLException sql) {
+                System.out.println(sql);
+            }
+        }
+        return null;
+    }
+
+    public Cliente obterPorCodigo(int codCliente) {
+        if ("sucesso".equals(conecta.getMsg())) {
+            try {
+                String sql = "SELECT * FROM APP.A1_CLIENTE WHERE COD_CLIENTE = " + codCliente;
+                ResultSet rs = conecta.getStm().executeQuery(sql);
+                rs.next();
+                Cliente cliente = new Cliente(rs.getInt("COD_CLIENTE"),
+                        rs.getString("NOME"),
+                        rs.getString("CPF"),
+                        rs.getString("RG"));
+                return cliente;
+            } catch (SQLException sql) {
+                System.out.println(sql);
+            }
+        }
+        return null;
+    }
 }
