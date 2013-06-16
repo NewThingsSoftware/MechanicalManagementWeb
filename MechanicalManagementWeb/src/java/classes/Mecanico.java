151416758a2b04fa1c2408f6bb8dbfe3f97db6a8
Marihelly Santini <marihelly_santini@hotmail.com>
2013-06-16 13:05:19 -0300

    Adicionado as DAOs :)

--- MechanicalManagementWeb/src/java/classes/Mecanico.java
+++ MechanicalManagementWeb/src/java/classes/Mecanico.java
@@ -1,13 +1,55 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package classes;
-
-/**
- *
- * @author Bruno
- */
 public class Mecanico {
+    
+    private int codMecanico;
+    private String nome, cpf, rg;
+
+    public Mecanico() {
+    }
+
+    public Mecanico(String nome, String cpf, String rg) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    public Mecanico(int codMecanico, String nome, String cpf, String rg) {
+        this.codMecanico = codMecanico;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+
+    public int getCodMecanico() {
+        return codMecanico;
+    }
+
+    public void setCodMecanico(int codMecanico) {
+        this.codMecanico = codMecanico;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
     
 }
